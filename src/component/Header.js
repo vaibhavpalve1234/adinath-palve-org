8fe773ff67b9d54497dc9957ded2995e036bc579
v.palve777 <57386502+vaibhavpalve1234@users.noreply.github.com>
2026-02-03 20:16:57 +0530

    Make politician content data-driven

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,6 +6,15 @@ import Footer from './Footer/Footer';
 import MultipleVideoPlayer from './platform/MultipleVideoPlayer';
 import Slider from './Slider/Slider';
 import PhotoGallery from './Photo/PhotoGallery';
+import {
+    heroSlides,
+    leaderHighlights,
+    videoHighlights,
+    galleryPhotos,
+    expandedGalleryPhotos,
+    footerContent,
+    appFooter,
+} from '../data/politicianData';
 
 export default function Header() {
     const opts = {
@@ -18,28 +27,28 @@ export default function Header() {
         }
     };
 
-    const videos = [
-        { src: '4AHew1QqXDo', opts: { ...opts, playerVars: { autoplay: 1, mute: 0, start: 50 , end:56, loop:1 } }, details:"main song" },
-        { src: 'yyrpJNcqHTA', opts, details:"संतोष नगर मैं शिवसेना शाखा प्रमुख की तरफ से जरूरतमंद 300 परिवारों को राशन सामग्री दी गई" },
-        { src: 'S7L4-JnQfiY', opts, details:"शिवसेनेचे शाखाप्रमुख आदिनाथ पालवे यांचा वाढदिवस मोठ्या उत्साहात साजरा." },
-        { src: 'GQlll2MI0wM', opts, details:"वाढदिवसाच्या शिवमय शुभेच्छा आदिनाथ पालवे जी" },
-        { src: '8lK-sxr9kmo', opts: { ...opts, playerVars: {  start:55 } }, details:"सर्वजनक गणेश उत्सव मंडल के प्रयासों से संतोष नगर मै 7 लाख रुपए की निधि से विकास कार्यों का भूमिपूजन" },
-        { src: 'g50DvfSgNew', opts, details:"धर्मवीर" },
-        { src: 'YlIc_kOQufw', opts, details:"सामुदायिक वाढदिवस सोहळा संतोषनगर उल्हासनगर ३" },
-        { src: 'eknr5tuiLHU', opts, details:"बाबा साहेब १३१ वॉं जयंती समारोह संतोषनगर उल्हासनगर ३" },
-        { src: 'sHJFKU7S41Y', opts, details:"उल्हासनगरमधील सेंट्रल रुग्णालयाची दुरावस्था." },
-    ];
+    const videos = videoHighlights.map((video) => ({
+        src: video.videoId,
+        opts: {
+            ...opts,
+            playerVars: {
+                ...opts.playerVars,
+                ...video.playerVars,
+            },
+        },
+        details: video.details,
+    }));
     return (
         <div>
             <div >
                 <HeaderContainer />
             </div>
             <div>
-                <Details />
+                <Details slides={heroSlides} />
             </div>
             <br />
             <div>
-                <Slider />
+                <Slider images={leaderHighlights} />
             </div>
             <br />
             <div>
@@ -47,16 +56,20 @@ export default function Header() {
             </div>
             <br />
             <div>
-                <PhotoGallery />
+                <PhotoGallery photos={galleryPhotos} expandPhotoes={expandedGalleryPhotos} />
             </div>
             <br/>
             <span></span>
             <div>
-                <Footer />
+                <Footer content={footerContent} />
             </div>
             <hr/>
             <div className="text-center">
-                <p> Copyright @ 2023. All Right Reserved. <strong>Adinath palve</strong> | Developed & design <strong>Vaibhav palve</strong></p>
+                <p>
+                    {appFooter?.copyrightText}{' '}
+                    <strong>{appFooter?.owner}</strong> | Developed & design{' '}
+                    <strong>{appFooter?.designer}</strong>
+                </p>
             </div>
         </div>
     )
